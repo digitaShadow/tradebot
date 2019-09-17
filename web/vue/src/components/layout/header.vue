97dffdc78e006f0b0401e710ed3d316177054c7c
digitaShadow <digitaShadow@users.noreply.github.com>
2019-09-18 02:21:47 +0500

    Update header.vue

--- web/vue/src/components/layout/header.vue
+++ web/vue/src/components/layout/header.vue
@@ -3,16 +3,15 @@
     #top
     header.bg--off-white.grd
       .contain.grd-row
-        h3.py1.px2.col-2 Gekko UI
+        h3.py1.px2.col-2 TradeBot UI
     nav.bg--light-gray
       .menu.contain
         router-link(to='/home').py1 Home
-        router-link(to='/live-gekkos').py1 Live Gekkos
+        router-link(to='/live-gekkos').py1 Live Bots
         router-link(to='/backtest').py1 Backtest
         router-link(to='/data').py1 Local data
         router-link(to='/config').py1 Config
-        a(href='https://gekko.wizb.it/docs/introduction/about_gekko.html', target='_blank').py1 Documentation
-
+        
 </template>
 
 <script>
