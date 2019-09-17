920c29d7e48f5a3c84a85544bcb2973b69b3356a
digitaShadow <digitaShadow@users.noreply.github.com>
2019-09-18 02:23:09 +0500

    Update home.vue

--- web/vue/src/components/layout/home.vue
+++ web/vue/src/components/layout/home.vue
@@ -2,9 +2,7 @@
   section.contain.grd-row
     .grd-row-col-3-6(v-html='left')
     .grd-row-col-3-6.txt--center
-      img(src='static/gekko.jpg')
-      p
-        em The most valuable commodity I know of is information.
+      
 </template>
 
 <script>
@@ -14,13 +12,10 @@ const left = marked(`
 
 ## Gekko
 
-Gekko is a Bitcoin trading bot and backtesting platform that
-connects to popular Bitcoin exchanges. It is written in javascript
-and runs on nodejs.
+This bot is a Bitcoin trading bot and backtesting platform that
+connects to popular Bitcoin exchanges. It supports multiple exchanges and you can use them by adding the api
+keys to your exchanges.
 
-[Find out more](https://gekko.wizb.it/).
-
-*Gekko is 100% free (open source), if you paid for this you have been scammed.*
 
 `);
 
